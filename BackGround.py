eb3685d7e93bd615ab77d5b91abbbb060bf52862
RichardPony <1607700419@qq.com>
2019-08-08 22:25:31 +0800

    Push

--- BackGround.py
+++ BackGround.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-login_head = '''Accept: */*
-Origin: http://cet-bm.neea.edu.cn
-X-Requested-With: XMLHttpRequest
-User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36
-Content-Type: application/x-www-form-urlencoded; charset=UTF-8
-Referer: http://cet-bm.neea.edu.cn/Home/QuickPrintTestTicket
-Accept-Encoding: gzip, deflate
-Accept-Language: zh-CN,zh;q=0.9'''
+login_head = '''Host:cet-bm.neea.edu.cn
+Connection:keep-alive
+Content-Length:111
+Accept:*/*
+Origin:http://cet-bm.neea.edu.cn
+X-Requested-With:XMLHttpRequest
+User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36
+Content-Type:application/x-www-form-urlencoded; charset=UTF-8
+Referer:http://cet-bm.neea.edu.cn/Home/QuickPrintTestTicket
+Accept-Encoding:gzip, deflate
+Accept-Language:zh-CN,zh;q=0.9'''
 def getHeaders(raw_head):
     headers = {}
     for raw in raw_head.split('\n'):
@@ -17,7 +20,8 @@ def getHeaders(raw_head):
     return headers
 
 Request_header = getHeaders(login_head)
-url = "http://cet-bm.neea.edu.cn/Home/QuickPrintTestTicket"
+print(Request_header)
+url = "http://cet-bm.neea.edu.cn/Home/"
 form_data = {
     'IDNumber': '371522200011047856',
     'IDTypeCode': '1',
@@ -26,9 +30,20 @@ form_data = {
     'verificationCode': ''
 }
 
-
+Page_url = url + "QuickPrintTestTicket"
 s = requests.session()
 
-page = s.get(url)
+page = s.get(Page_url)
+
+pic = s.get(url + 'VerifyCodeImg').content
+with open('Ver_CodeImg.png', 'wb') as f:
+    f.write(pic)
+
+ver_code = input("请输入验证码：")
+form_data['verificationCode'] = ver_code
+print(form_data)
 # Page_Detail = BeautifulSoup(page.text, 'lxml')
-print(page.text)
+res = s.post(Page_url, form_data, headers = Request_header)
+
+# print(page.text)
+print(res.text)
